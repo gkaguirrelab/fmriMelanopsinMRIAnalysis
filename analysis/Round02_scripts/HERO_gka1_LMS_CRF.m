057f61bef147f1213434ad2dde797d541835865f
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2016-06-14 09:56:26 -0400

    New processing scripts added

---
+++ analysis/Round02_scripts/HERO_gka1_LMS_CRF.m
@@ -0,0 +1,26 @@
+%%%%%%%% Analysis for HERO_gka1 LMS_CRF %%%%%%%%%%
+
+%% inputs
+results_dir =  '/data/jag/MELA/MelanopsinMR/Results';
+data_dir = '/data/jag/MELA/MelanopsinMR'; %Upenn cluster default path
+SUBJECTS_DIR = '/data/jag/MELA/freesurfer_subjects'; %Upenn cluster default path
+subject_name = 'HERO_gka1_3T';
+subj_name = 'HERO_gka1';
+session_date = '060616';
+condition = 'LMS_CFR';
+numOfRuns =  10 ;
+reconall = 0;  %already done
+funcs = { ...
+'wdrf.tf' ...
+'s5.wdrf.tf' ...
+};
+proj_template = true;
+proj_copes = true;
+
+
+%% Step 1: preprocessing
+
+MelanopsinMR_Preprocessing (results_dir, data_dir, SUBJECTS_DIR, ...
+    subject_name, subj_name, session_date, condition, numOfRuns, reconall)
+
+% run the sh script and proceed to step 2
---
+++ analysis/Round02_scripts/HERO_mxs1_Mel_CRF_061016.m
@@ -0,0 +1,26 @@
+%%%%%%%% Analysis for HERO_mxs1 Mel_CFR(061016) %%%%%%%%%%
+
+%% inputs
+results_dir =  '/data/jag/MELA/MelanopsinMR/Results';
+data_dir = '/data/jag/MELA/MelanopsinMR'; %Upenn cluster default path
+SUBJECTS_DIR = '/data/jag/MELA/freesurfer_subjects'; %Upenn cluster default path
+subject_name = 'HERO_mxs1_MaxMel';
+subj_name = 'HERO_mxs1';
+session_date = '061016_Mel';
+condition = 'Mel_CFR';
+numOfRuns =  4 ;
+reconall = 0;  %already done
+funcs = { ...
+'wdrf.tf' ...
+'s5.wdrf.tf' ...
+};
+proj_template = true;
+proj_copes = true;
+
+
+%% Step 1: preprocessing
+
+MelanopsinMR_Preprocessing (results_dir, data_dir, SUBJECTS_DIR, ...
+    subject_name, subj_name, session_date, condition, numOfRuns, reconall)
+
+% run the sh script and proceed to step 2
