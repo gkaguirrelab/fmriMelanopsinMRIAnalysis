6350548c105cf7027ac39084ff7300346a781ca2
Giulia Frazzetta <giulia.frazzetta@gmail.com>
2016-06-16 17:05:28 -0400

    Scripts updates

--- analysis/Round02_scripts/HERO_aso1_LMS_CFR.m
+++ analysis/Round02_scripts/HERO_aso1_LMS_CFR.m
@@ -7,7 +7,7 @@ SUBJECTS_DIR = '/data/jag/MELA/freesurfer_subjects'; %Upenn cluster default path
 subject_name = 'HERO_aso1_MaxMel';
 subj_name = 'HERO_aso1';
 session_date = '060116';
-condition = 'LMS_CFR';
+condition = 'MaxLMSCFR';
 numOfRuns =  9 ;
 reconall = 0;  %already done
 funcs = { ...
@@ -24,3 +24,8 @@ MelanopsinMR_Preprocessing (results_dir, data_dir, SUBJECTS_DIR, ...
     subject_name, subj_name, session_date, condition, numOfRuns, reconall)
 
 % run the sh script and proceed to step 2
+
+%% Step 2: first level stat
+
+MelanopsinMR_FeatStatAnalysis (results_dir, data_dir, subj_name,session_date,condition, numOfRuns, funcs, SUBJECTS_DIR)
+% run the sh scripts and proceed to step 3
