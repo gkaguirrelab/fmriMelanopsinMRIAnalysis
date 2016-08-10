bcf80e2f16661cf58e14db58bad4f8408baa3683
Giulia Frazzetta <gfrazzetta@chead.uphs.upenn.edu>
2016-08-10 16:05:02 -0400

    MASTER script debugged

--- analysis/Round02_scripts/cleanup_script.m
+++ analysis/Round02_scripts/cleanup_script.m
@@ -1,16 +1,1 @@
 data_dir = '/data/jag/MELA/MelanopsinMR/';
-
-subjNames = { ...
-    'HERO_asb1' ...
-    'HERO_aso1' ...
-    'HERO_gka1' ...
-    'HERO_mxs1' ...
-    };
-
-for nn = 1:length(subjNames)
-    sessions = listdir(fullfile(data_dir, subjNames{nn}),'dirs');
-    for ss = 1:length(sessions)
-        session_dir = fullfile(data_dir, subjNames{nn}, sessions{ss});
-        clean_up(session_dir)
-    end
-end
