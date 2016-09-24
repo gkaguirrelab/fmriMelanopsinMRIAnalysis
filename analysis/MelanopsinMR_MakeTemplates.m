bbad9b0438de71467110f8863e62bb6de4148a24
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-24 08:54:45 -0700

    Updates
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- analysis/MelanopsinMR_MakeTemplates.m
+++ analysis/MelanopsinMR_MakeTemplates.m
@@ -21,22 +21,38 @@ templateFiles = {fullfile(anatTemplateDir, 'eccen-template-2.5.sym.mgh') ...
     fullfile(anatTemplateDir, 'areas-template-2.5.sym.mgh')};
 
 % HERO_asb1
-sessionDir = '/data/jag/MELA/MelanopsinMR/HERO_asb1';
+subjDir = '/data/jag/MELA/MelanopsinMR/HERO_asb1';
 subjectName = 'HERO_asb1_MaxMel';
-project_template(sessionDir, subjectName, templateFiles);
+project_template(fullfile(subjDir, '032416'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '040716'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '051016'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '060716'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '060816'), subjectName, templateFiles);
 
 % HERO_aso1
-sessionDir = '/data/jag/MELA/MelanopsinMR/HERO_aso1';
+subjDir = '/data/jag/MELA/MelanopsinMR/HERO_aso1';
 subjectName = 'HERO_aso1_MaxMel';
-project_template(sessionDir, subjectName, templateFiles);
+project_template(fullfile(subjDir, '032516'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '033016'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '042916'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '053116'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '060116'), subjectName, templateFiles);
 
 % HERO_gka1
-sessionDir = '/data/jag/MELA/MelanopsinMR/HERO_mxs1';
+subjDir = '/data/jag/MELA/MelanopsinMR/HERO_mxs1';
 subjectName = 'HERO_gka1_MaxMel';
-project_template(sessionDir, subjectName, templateFiles);
+project_template(fullfile(subjDir, '033116'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '040116'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '050616'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '060216'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '060616'), subjectName, templateFiles);
 
 % HERO_mxs1
-sessionDir = '/data/jag/MELA/MelanopsinMR/HERO_mxs1';
+subjDir = '/data/jag/MELA/MelanopsinMR/HERO_mxs1';
 subjectName = 'HERO_mxs1_MaxMel';
-project_template(sessionDir, subjectName, templateFiles);
-
+project_template(fullfile(subjDir, '040616'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '040816'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '050916'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '060916'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '061016_Mel'), subjectName, templateFiles);
+project_template(fullfile(subjDir, '062816'), subjectName, templateFiles);
