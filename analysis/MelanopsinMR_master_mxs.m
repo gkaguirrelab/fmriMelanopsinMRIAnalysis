62add00ebeb3de86391a746580ef8f6c39436920
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-24 08:11:34 -0700

    Updates
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

---
+++ analysis/MelanopsinMR_master_mxs.m
@@ -0,0 +1,13 @@
+% New Master from MS
+
+% Create preprocessing scripts
+MelanopsinMR_Analysis_400PctData;
+MelanopsinMR_Analysis_CRFData;
+MelanopsinMR_Analysis_SplatterControlData;
+
+
+% Make anatomical templates
+MelanopsinMR_MakeTemplates;
+
+% Derive the HRFs
+MelanopsinMR_HRF;
