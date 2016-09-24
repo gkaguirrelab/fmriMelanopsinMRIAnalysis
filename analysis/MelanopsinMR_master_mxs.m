5157669bbc5709ca8dc91d5ca84cecf1b9246594
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-24 19:18:17 -0400

    Updates

--- analysis/MelanopsinMR_master_mxs.m
+++ analysis/MelanopsinMR_master_mxs.m
@@ -1,13 +1,15 @@
 % New Master from MS
+results_dir =  '/data/jag/MELA/MelanopsinMR/Results';
+data_dir = '/data/jag/MELA/MelanopsinMR'; %Upenn cluster default path
 
 % Create preprocessing scripts
 MelanopsinMR_Analysis_400PctData;
 MelanopsinMR_Analysis_CRFData;
 MelanopsinMR_Analysis_SplatterControlData;
 
-
 % Make anatomical templates
 MelanopsinMR_MakeTemplates;
 
 % Derive the HRFs
-MelanopsinMR_HRF;
+MelanopsinMR_HRFDerive;
+MelanopsinMR_HRFPlot;
