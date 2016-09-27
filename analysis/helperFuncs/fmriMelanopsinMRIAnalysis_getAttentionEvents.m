b6a10b7251512ec80da4d715d9514f4da02a0b7c
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-27 14:04:43 -0400

    Made a function to extract attention events
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

---
+++ analysis/helperFuncs/fmriMelanopsinMRIAnalysis_getAttentionEvents.m
@@ -0,0 +1,23 @@
+function eventTimes = fmriMelanopsinMRIAnalysis_getAttentionEvents(params)
+% eventTimes = fmriMelanopsinMRIAnalysis_makeStimStruct(params)
+%
+%  Makes a stimulus structure.
+%
+% 9/27/2016     ms      Homogenized comments and function documentation.
+%                       Based on code from Andrew S. Bock (subjHRF.m)
+
+% Get the attention events
+stimulus.metaData           = load(params.stimulusFile);
+ct = 0;
+attEvents = [];
+for j = 1:size(stimulus.metaData.params.responseStruct.events,2)
+    % Get the attention events
+    if sum(strcmp(stimulus.metaData.params.responseStruct.events(j).describe.direction,attentionTaskNames))
+        ct = ct + 1;
+        % Get the stimulus window
+        attEvents(ct) = stimulus.metaData.params.responseStruct.events(j).tTrialStart - ...
+            stimulus.metaData.params.responseStruct.tBlockStart + ...
+            stimulus.metaData.params.thePhaseOffsetSec(stimulus.metaData.params.thePhaseIndices(j));
+    end
+end
+eventTimes                  = round(attEvents*1000); % attention events (msec)
