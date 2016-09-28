f75f07fb1ca8645d11b46df508764509a0f23261
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-28 14:24:55 -0400

    Further work on packetFit

--- analysis/fmriMelanopsinMRIAnalysis_packetFit.m
+++ analysis/fmriMelanopsinMRIAnalysis_packetFit.m
@@ -10,6 +10,7 @@ function fmriMelanopsinMRIAnalysis_packetFit(inputParams)
 temporalFit = tfeIAMP('verbosity','none');
 params0 = temporalFit.defaultParams();
 paramLockMatrix = [];
+defaultParamsInfo.nInstances = 1;
 
 %% Set initial params
 params                  = inputParams;
@@ -27,6 +28,9 @@ params.respTimeBase     = 0:TR:(runDur*TR)-TR;
 % Extract only the stimulus
 params.stimValues = sum(params.stimValues(find(params.stimMetaData.stimTypes == 1), :));
 
+% Mean center the stimulus
+params.stimValues = params.stimValues - mean(params.stimValues);
+
 %% Extract the attention events
 eventTimes = fmriMelanopsinMRIAnalysis_getAttentionEvents(params);
 
@@ -39,6 +43,8 @@ volDims                 = size(resp.vol);
 flatVol                 = reshape(resp.vol,volDims(1)*volDims(2)*volDims(3),volDims(4));
 flatVolPSC              = NaN*zeros(size(flatVol));
 cleanDataPSC            = NaN*zeros(size(flatVol));
+fitAmp                  = NaN*zeros(size(flatVol, 1));
+fitErr                  = NaN*zeros(size(flatVol, 1));
 
 %% If 'bold', get HRF
 if strcmp(params.packetType,'bold')
@@ -46,25 +52,33 @@ if strcmp(params.packetType,'bold')
 end
 
 %% Iterate over all voxels
+tic;
 for ii = 1:size(flatVol, 1)
-    % Convert to % signal change, and remove the HRF
-    flatVolPSC(ii, :)             = convert_to_psc(flatVol(ii, :));
-    [~, cleanDataPSC(ii, :)]      = deriveHRF(flatVolPSC(ii, :)',eventTimes,TR*1000,HRFdur,numFreqs);
-    
-    % Re-center the data
-    % <?> s
-    
-    % Make a packet
-    params.respValues             = cleanDataPSC(ii, :);
-    thePacket                     = makePacket(params);
-    
-    % Fit packet here
-    [paramsFit,fVal,modelResponseStruct] = ...
-        temporalFit.fitResponse(thePacket,...
-        'defaultParamsInfo', defaultParamsInfo, ...
-        'paramLockMatrix',paramLockMatrix, ...
-        'searchMethod','linearRegression');
+    if ~all(flatVol(ii, :) == 0)
+        % Convert to % signal change, and remove the HRF
+        flatVolPSC(ii, :)             = convert_to_psc(flatVol(ii, :));
+        [~, cleanDataPSC(ii, :)]      = deriveHRF(flatVolPSC(ii, :)',eventTimes,TR*1000,HRFdur,numFreqs);
+        
+        % Re-center the data
+        cleanDataPSC(ii, :) = cleanDataPSC(ii, :) - mean(cleanDataPSC(ii, :));
+        
+        % Only fit if we actually have non-NaN data
+        
+        % Make a packet
+        params.respValues             = cleanDataPSC(ii, :);
+        thePacket                     = makePacket(params);
+        
+        % Fit packet here
+        [paramsFit,fVal,modelResponseStruct] = ...
+            temporalFit.fitResponse(thePacket, ...
+            'defaultParamsInfo', defaultParamsInfo, ...
+            'paramLockMatrix', paramLockMatrix, ...
+            'searchMethod','linearRegression');
+        fitAmp(ii) = paramsFit.paramMainMatrix(1);
+        fitErr(ii) = fVal;
+    else
+        fitAmp(ii) = NaN;
+        fitErr(ii) = NaN;
+    end
 end
-
-
-
+toc;
