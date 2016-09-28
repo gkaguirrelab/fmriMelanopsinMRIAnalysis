c1aa96a82907cf5bf880d36c4dd6b74526a0e4e0
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-28 11:10:04 -0400

    Made the outer loop
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

---
+++ analysis/fmriMelanopsinMRIAnalysis_fit400PctData.m
@@ -0,0 +1,18 @@
+%% Iterate over the subjects
+subList = listdir(fullfile(inputParams.dataDir,'HERO_*'),'dirs');
+for ss = 1:length(subList) % Iterate over subjects
+    
+    % Figure out how many sessions we have
+    sessList = listdir(fullfile(inputParams.dataDir,subList{ss}),'dirs');
+    
+    %% Iterate over the sessions
+    for sn = 1:length(sessList);
+       
+        inputParams.stimulusFile = fullfile(params.sessionDir,'MatFiles/HERO_asb1-MelanopsinMRMaxMel-01.mat');
+        inputParams.responseFile = fullfile(params.sessionDir,'Series_012_fMRI_MaxMelPulse_A_AP_run01/wdrf.tf.nii.gz');
+        
+        fmriMelanopsinMRIAnalysis_packetFit(inputParams)
+        
+    end
+end
+
