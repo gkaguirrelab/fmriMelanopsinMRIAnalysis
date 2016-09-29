0b888c0c0d16d9f669b93a6605482a07539b75a5
Manuel Spitschan <mspits@sas.upenn.edu>
2016-09-29 16:40:05 -0400

    Update
    Signed-off-by: Manuel Spitschan <mspits@sas.upenn.edu>

--- analysis/fmriMelanopsinMRIAnalysis_fit400PctData.m
+++ analysis/fmriMelanopsinMRIAnalysis_fit400PctData.m
@@ -1,21 +1,26 @@
 %% Iterate over the subjects
 inputParams.dataDir = '/data/jag/MELA/MelanopsinMR';
 
-%% HERO_asb1 
-subjectID = 'HERO_asb1'; sessDir = '032416';
-inputParams.stimulusFile = fullfile(inputParams.sessionDir,'MatFiles/HERO_asb1-MelanopsinMRMaxMel-01.mat');
-inputParams.responseFile = fullfile(inputParams.sessionDir,'Series_012_fMRI_MaxMelPulse_A_AP_run01/wdrf.tf.nii.gz');
-inputParams.anatRefRun = fullfile(inputParams.sessionDir,'Series_012_fMRI_MaxMelPulse_A_AP_run01');
-fmriMelanopsinMRIAnalysis_packetFit(inputParams);
-
-
-    %% Iterate over the sessions
-    for sn = 1:length(sessList);
-        inputParams.sessionDir = '/data/jag/MELA/MelanopsinMR/HERO_asb1/032416/';
-        inputParams.stimulusFile = fullfile(inputParams.sessionDir,'MatFiles/HERO_asb1-MelanopsinMRMaxMel-01.mat');
-        inputParams.responseFile = fullfile(inputParams.sessionDir,'Series_012_fMRI_MaxMelPulse_A_AP_run01/wdrf.tf.nii.gz');
-        inputParams.anatRefRun = fullfile(inputParams.sessionDir,'Series_012_fMRI_MaxMelPulse_A_AP_run01');
-        fmriMelanopsinMRIAnalysis_packetFit(inputParams);
-    end
+%% HERO_asb1
+subjID = 'HERO_asb1'; sessionID = '032416';
+sessionDir = fullfile(inputParams.dataDir, subjID, sessionID);
+boldDirs = find_bold(sessionDir);
+matDir                          = fullfile(sessionDir,'MatFiles');
+matFiles                        = listdir(matDir,'files');
+for b = 1:length(boldDirs)
+    inputParams.stimulusFile = fullfile(matDir, matFiles{b});
+    inputParams.responseFile = fullfile(sessionDir, boldDirs{b}, 'wdrf.tf.nii.gz');
+    inputParams.anatRefRun = fullfile(sessionDir, boldDirs{1});
+    fmriMelanopsinMRIAnalysis_packetFit(inputParams);
 end
 
+inputParams.sessionDir       = fullfile(inputParams.dataDir, 'HERO_asb1/040716');
+
+inputParams.sessionDir       = fullfile(inputParams.dataDir, 'HERO_aso1/032516');
+inputParams.sessionDir       = fullfile(inputParams.dataDir, 'HERO_aso1/033016');
+
+inputParams.sessionDir       = fullfile(inputParams.dataDir, 'HERO_gka1/033116');
+inputParams.sessionDir       = fullfile(inputParams.dataDir, 'HERO_gka1/040116');
+
+inputParams.sessionDir       = fullfile(inputParams.dataDir, 'HERO_mxs1/040616');
+inputParams.sessionDir       = fullfile(inputParams.dataDir, 'HERO_mxs1/040816');
