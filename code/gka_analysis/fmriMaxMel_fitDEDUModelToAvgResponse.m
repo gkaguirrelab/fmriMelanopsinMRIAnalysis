eb4bce810d915ae7c8f549852f81cee369c01413
gkaguirre <gkaguirre@me.com>
2017-02-14 10:45:00 -0500

    Working on bootstrap fits of DEDU model
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/gka_analysis/fmriMaxMel_fitDEDUModelToAvgResponse.m
+++ code/gka_analysis/fmriMaxMel_fitDEDUModelToAvgResponse.m
@@ -1,8 +1,9 @@
 function [durationArray, plotHandle] = fmriMaxMel_fitDEDUModelToAvgResponse(meanEvokedResponsesCellArray, kernelStructCellArray, exptIdx, stimulusIdx)
-% function [delayArray, plotHandle] = fmriMaxMel_fitDEDUModelToAvgResponse(meanEvokedResponsesCellArray, kernelStructCellArray, exptIdx, stimulusIdx)
 %
 
 verbosity='none';
+
+stimulusDeltaT=1; % time resolution of the stimulus model, in msecs
 
 % Construct the model object
 tfeHandle = tfeDEDU('verbosity','none');
@@ -13,18 +14,43 @@ nSubjects=size(kernelStructCellArray,2);
 plotHandle=figure();
 
 for ss=1:nSubjects
+
+   % Build a packet with a mean response and an impulse stimulus
+
     thePacket.response = meanEvokedResponsesCellArray{exptIdx}{ss,stimulusIdx};
     thePacket.kernel = prepareHRFKernel(kernelStructCellArray{ss});
-    thePacket.stimulus = thePacket.response;
-    thePacket.stimulus.values = thePacket.stimulus.values*0;
+    check=diff(thePacket.response.timebase);
+    responseDeltaT=check(1);
+    totalDuration=max(thePacket.response.timebase)+responseDeltaT;
+    thePacket.stimulus.timebase=0:stimulusDeltaT:totalDuration-stimulusDeltaT;
+    thePacket.stimulus.values=thePacket.stimulus.timebase*0;
     thePacket.stimulus.values(1) = 1;
-    thePacket.metaData = [];
+    
+   % Build a packetCellArray by copying over the run-by-run responses into the model packet
+   runResponses=thePacket.response.runs;
+   nRuns=size(runResponses,1);
+   for rr=1:nRuns
+   packetCellArray{rr}=thePacket;
+   theResponse=runResponses(rr,:);
+   % set the mean of the initial value to zero across runs
+   theResponse=theResponse-thePacket.response.values(1);
+   packetCellArray{rr}.response.values=theResponse;
+   end
+   
+    
+    
     
     defaultParamsInfo.nInstances=1;
-    [paramsFit,fVal,modelResponseStruct] = ...
-        tfeHandle.fitResponse(thePacket,...
-        'defaultParamsInfo', defaultParamsInfo, ...
-        'DiffMinChange',0.001);
+
+         [ xValFitStructure, averageResponseStruct, modelResponseStruct ] = crossValidateFits( subPacketCellArray, tfeHandle, ...
+             'partitionMethod','bootstrap', ...
+             'maxPartitions',100, ...
+             'aggregateMethod', 'mean',...
+             'verbosity', 'none',...
+             'errorType', '1-r2', ...
+             'DiffMinChange', 0.001,...
+             'verbosity','full');
+
     subplot(4,1,ss)
     tfeHandle.plot(thePacket.response,'NewWindow',false,'DisplayName','Data');
     hold on
